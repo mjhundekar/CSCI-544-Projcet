4334c80281a10729affc5b2b57092f0d0773f3a6
ankit081190 <ankitkothari17@yahoo.com>
2017-04-10 06:07:34 -0700

    Final result

--- Sentiment/topicSentiments.py
+++ Sentiment/topicSentiments.py
@@ -5,14 +5,15 @@ sys.setdefaultencoding("utf-8")
 
 
 def getValues(word):
+    #print "word================>",word,"================"
     inFilePath="/home/ankit/NLP_Project/CSCI-544-Projcet/SentimentResults/"
     d = defaultdict(int)
     fileList=os.listdir(inFilePath)
-    
+
     for files in fileList:
-        with codecs.open(inFilePath+files) as f:
+        with codecs.open(inFilePath+files,"r", "utf-8") as f:
             for line in f:
-                if word in line:
+                if word.decode("utf-8").strip() in line.strip().split(" "):
                     d[line.split(" :::: ")[0]] +=1
             f.close()
     return d;
@@ -21,9 +22,12 @@ with codecs.open("topics.txt", "r", "utf-8") as f:
     with codecs.open("topicSentiment.txt", "w", "utf-8") as fopen:
         for line in f:
             if ":" in line:
-                print line.split(":")[1].decode("utf-8")
-                dict=getValues(line.split(":")[1].decode("utf-8"))
-                fopen.write(str(dict)+"\n")
+                #print line.split(":")[1].decode("utf-8")
+                dict=getValues(line.split(":")[1].decode("utf-8").strip())
+                dictStr=""
+		for i, j in dict.items():
+                    dictStr+=" :: "+str(i)+" : "+str(j)
+                fopen.write(line.split(":")[1].decode("utf-8").strip()+" \t "+dictStr+"\n")
         fopen.close()
     f.close()
 
