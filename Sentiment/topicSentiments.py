7f8660b73f020fe404e6f5d699b69c803e3b43a3
ankit081190 <ankitkothari17@yahoo.com>
2017-04-28 13:46:53 +0000

    Final

--- Sentiment/topicSentiments.py
+++ Sentiment/topicSentiments.py
@@ -7,15 +7,19 @@ sys.setdefaultencoding("utf-8")
 def getValues(word):
     #print "word================>",word,"================"
     inFilePath="/home/ankit081190/NLP/CSCI-544-Project/SentimentResultsFinal/"
-    d = defaultdict(int)
+    d = {}
+#    dated=defaultdict(int)
+
     fileList=os.listdir(inFilePath)
 #    print fileList
    
     for files in fileList:
+        dated=defaultdict(int)
         with codecs.open(inFilePath+files,"r", "utf-8") as f:
             for line in f:
                 if word.decode("utf-8").strip() in line.strip().split(" "):
-                    d[line.split(" :::: ")[0]] +=1
+                    dated[line.split(" :::: ")[0]]+=1
+                    d[word +", "+files.replace(".txt", "")] = dated
             f.close()
     return d;
 
@@ -25,10 +29,14 @@ with codecs.open("topics.txt", "r", "utf-8") as f:
             if ":" in line:
                 #print line.split(":")[1].decode("utf-8")
                 dict=getValues(line.split(":")[0].decode("utf-8").strip())
+                print dict
                 dictStr=""
-		for i, j in dict.items():
-                    dictStr+=" :: "+str(i)+" : "+str(j)
-                fopen.write(line.split(":")[0].decode("utf-8").strip()+" \t "+dictStr+"\n")
+		for topic, datedict in sorted(dict.items()):
+                    for i, j in sorted(datedict.items()):
+                        print "topic,i,j=",topic,i,j
+                        dictStr+=" :: "+str(i)+" : "+str(j)
+#                        fopen.write(line.split(":")[0].decode("utf-8").strip()+" \t "+dictStr+"\n")
+                        fopen.write(topic.strip()+" "+ str(i) +" "+ str(j) +"\n")
         fopen.close()
     f.close()
 
