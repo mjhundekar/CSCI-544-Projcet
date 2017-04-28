c264dc3399eda73272e545e659ac7c8ecc958519
ankit081190 <ankitkothari17@yahoo.com>
2017-04-28 13:43:44 +0000

    Final

--- DynamicLDA/dynamicLDA.py
+++ DynamicLDA/dynamicLDA.py
@@ -41,64 +41,64 @@ for files in fileList:
 
 
 
-#time seq required for DTM (Should contain all 1s for our tweets case)
-#time_seq = [3, 7]
-time_seq=[];
-for i in range(1,len(documents)+1):
-    time_seq.append(1);
+    #time seq required for DTM (Should contain all 1s for our tweets case)
+    #time_seq = [3, 7]
+    time_seq=[];
+    for i in range(1,len(documents)+1):
+        time_seq.append(1);
 
 
-#Corpus class for DTM data load
-class DTMcorpus(corpora.textcorpus.TextCorpus):
-    def get_texts(self):
-        return self.input
-    def __len__(self):
-        return len(self.input)
+    #Corpus class for DTM data load
+    class DTMcorpus(corpora.textcorpus.TextCorpus):
+        def get_texts(self):
+            return self.input
+        def __len__(self):
+            return len(self.input)
 
-#corpus = DTMcorpus(documents)
-corpus=documents
-dictionary=Dictionary(corpus)
+    #corpus = DTMcorpus(documents)
+    corpus=documents
+    dictionary=Dictionary(corpus)
 
-corpus = [dictionary.doc2bow(doc) for doc in corpus]
-for (token, uid) in dictionary.token2id.items():
-        dictionary.id2token[uid] = token
+    corpus = [dictionary.doc2bow(doc) for doc in corpus]
+    for (token, uid) in dictionary.token2id.items():
+            dictionary.id2token[uid] = token
 
-print type(dictionary), type(corpus)
+    print type(dictionary), type(corpus)
 
-#path where dtm file is installed
-dtm_path="/home/ankit081190/NLP/dtm/dtm/dtm"
+    #path where dtm file is installed
+    dtm_path="/home/ankit081190/NLP/dtm/dtm/dtm"
 
-#model = DtmModel(dtm_path, corpus, time_seq, num_topics=1,
-#                 id2word=corpus.dictionary, initialize_lda=True)
+    #model = DtmModel(dtm_path, corpus, time_seq, num_topics=1,
+    #                 id2word=corpus.dictionary, initialize_lda=True)
 
-model=LdaMulticore(corpus, num_topics=10, id2word=dictionary)
+    model=LdaMulticore(corpus, num_topics=10, id2word=dictionary)
 
-model.save("DTModelMultiCore.txt")
-#Gives top 25 topics
+    model.save("DTModelMultiCore_"+files+".model")
+    #Gives top 25 topics
 
-tp= model.show_topics(num_topics=25, log=False, formatted=True)
-print model.print_topics(num_topics=25)
-data = pyLDAvis.gensim.prepare(model, corpus, dictionary)
-pyLDAvis.save_html(data, 'index_lda.html')
+    tp= model.show_topics(num_topics=25, log=False, formatted=True)
+    print model.print_topics(num_topics=25)
+    data = pyLDAvis.gensim.prepare(model, corpus, dictionary)
+    pyLDAvis.save_html(data, 'index_lda_'+files+'.html')
 
-cnt= Counter(tp)
-with codecs.open("topicsMultiLDA.txt","w", "utf-8") as f:
-    for i,j in cnt:
-        print i,j;
-        f.write("\nFor Topic Number "+str(i)+":\n"+str(j).decode("utf-8")+"\n")
-    f.close()
+    cnt= Counter(tp)
+    with codecs.open("topicsMultiLDA"+files+".txt","w", "utf-8") as f:
+        for i,j in cnt:
+            print i,j;
+            f.write("\nFor Topic Number "+str(i)+":\n"+str(j).decode("utf-8")+"\n")
+        f.close()
 
-#for i, j in cnt:
-#    print "\nFor topic number: " ,i, "\n"; 
-#    print j.decode("utf-8")
-#for i in range(0,model.num_topics-1)):
-#    print model.show_topic
+    #for i, j in cnt:
+    #    print "\nFor topic number: " ,i, "\n"; 
+    #    print j.decode("utf-8")
+    #for i in range(0,model.num_topics-1)):
+    #    print model.show_topic
     
     
-#topics = model.show_topic(topicid=0, time=1, num_words=100)
-#cnt= Counter(topics)
-#with codecs.open("topicsMultiLDA.txt","w", "utf-8") as f:
-#    for i,j in cnt:
-#        print i,j;
-#        f.write(str(i)+":"+str(j).decode("utf-8")+"\n")
-#    f.close()
+    #topics = model.show_topic(topicid=0, time=1, num_words=100)
+    #cnt= Counter(topics)
+    #with codecs.open("topicsMultiLDA.txt","w", "utf-8") as f:
+    #    for i,j in cnt:
+    #        print i,j;
+    #        f.write(str(i)+":"+str(j).decode("utf-8")+"\n")
+    #    f.close()
--- PreProcess/removeWordsTFIDF.py
+++ PreProcess/removeWordsTFIDF.py
@@ -18,10 +18,10 @@ with codecs.open("/home/ankit081190/NLP/CSCI-544-Project/Model/tfIdf.txt","r", "
 #    print key, value
 #exit(1)
 
-fileList=os.listdir("/home/ankit081190/NLP/CSCI-544-Project/DayWiseTfIdfCleanTweets/")
-file_path = "/home/ankit081190/NLP/CSCI-544-Project/DayWiseTfIdfCleanTweets/"
-tfIdf_file_path = "/home/ankit081190/NLP/CSCI-544-Project/DayWiseTfIdfCleanTweets/Cleaned/"
-#fileList=["trim_2017-04-09.txt"]
+fileList=os.listdir("/home/ankit081190/NLP/CSCI-544-Project/DayWiseTfIdfCleanTweets/toBeAnnotated/")
+file_path = "/home/ankit081190/NLP/CSCI-544-Project/DayWiseTfIdfCleanTweets/toBeAnnotated/"
+tfIdf_file_path = "/home/ankit081190/NLP/CSCI-544-Project/DayWiseTfIdfCleanTweets/toBeAnnotated/"
+fileList=["2017-04-09.txt", "2017-04-10.txt", "2017-04-12.txt", "2017-04-18.txt", "2017-04-15.txt"]
 for files in fileList:
     docText=""
     if ".txt" in files:
@@ -29,8 +29,8 @@ for files in fileList:
 	with open(file_path + files,'r') as fp:
 	    for line in fp:
 		#print "line=",line
-
-                d=line.strip(" ").strip("\n").split(" ")
+		senti=(line.split(" ::: "))[1]
+                d=((line.split(" ::: "))[0]).strip(" ").strip("\n").split(" ")
                 t=1;
 		for words in d:
 #                    print words
@@ -40,10 +40,10 @@ for files in fileList:
     			    docText+=words.strip().decode("utf-8")+" ";
                             t=0;
                 if t==0:
-		    docText+="\n"
+		    docText+=" ::: "+senti
 		#print "docText=",docText
 
-	    with codecs.open(tfIdf_file_path + files,'w', 'utf-8') as fopen:
+	    with codecs.open(tfIdf_file_path +"Annotated_"+ files,'w', 'utf-8') as fopen:
 		fopen.write(docText.decode("utf-8"))
 		docText=""
 		fopen.close()
